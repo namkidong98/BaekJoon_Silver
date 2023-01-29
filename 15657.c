0ab997274791504bd9826aa7e2f021f3ed9973a2
namkidong98 <113520117+namkidong98@users.noreply.github.com>
2023-01-29 21:29:06 +0900

    2023-01-29

---
+++ 15657.c
@@ -0,0 +1,42 @@
+//N과 M 8 (실버 3)
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int N, M;
+int number[8];
+int target[8];
+
+void sequence(int count, int current) {
+	if (count == M) {
+		for (int i = 0; i < M; i++) printf("%d ", target[i]);
+		printf("\n");
+	}
+
+	else {
+		for (int i = current; i < N; i++) {
+			target[count] = number[i];
+			sequence(count + 1, i);
+		}
+	}
+}
+
+int main() {
+	int temp;
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &number[i]);
+	}
+
+	//오름차순으로 정렬
+	for (int i = 0; i < N - 1; i++) {
+		for (int j = i + 1; j < N; j++) {
+			if (number[i] > number[j]) {
+				temp = number[i];
+				number[i] = number[j];
+				number[j] = temp;
+			}
+		}
+	}
+
+	sequence(0, 0);
+}
